9ef8b9d8513b1897b4474e9c0c600d41531e109f
PackHubTH <aussadawut2545@gmail.com>
2022-11-04 02:56:00 +0700

    Feat: add js features and style

---
+++ src/auardrit/ContactParallax.js
@@ -0,0 +1,9 @@
+const parallax = (e) => {
+  document.querySelectorAll('.layer').forEach(layer => {
+    let speed = layer.getAttribute('data-speed')
+    const x = ((e.pageX - window.innerWidth / 2) * speed) / 100
+    const y = ((e.pageY - window.innerHeight / 2) * speed) / 100
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+  });
+}
+document.addEventListener("mousemove", parallax);
