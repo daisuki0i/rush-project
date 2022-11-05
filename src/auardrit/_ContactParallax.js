f25fe1edd40ff0194233e210ee51b7c292b597b1
PackHubTH <aussadawut2545@gmail.com>
2022-11-05 19:39:48 +0700

    Chore: responsive and add others

--- src/auardrit/_ContactParallax.js
+++ src/auardrit/_ContactParallax.js
@@ -2,8 +2,9 @@ const parallax = (e) => {
   document.querySelectorAll('.layer').forEach(layer => {
     let speed = layer.getAttribute('data-speed')
     const x = ((e.pageX - window.innerWidth / 2) * speed) / 100
-    const y = ((e.pageY - window.innerHeight / 2) * speed) / 100
+    const y = ((e.pageY - window.innerHeight / 2 - 3000) * speed) / 100
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    // console.log(e.pageX, e.pageY, window.innerHeight)
   });
 }
 document.addEventListener("mousemove", parallax);
---
+++ src/auardrit/_SkillAnimate.js
@@ -0,0 +1,30 @@
+AOS.init();
+
+const progress = () => {
+  console.log(window.scrollY)
+  document.querySelectorAll('.progress').forEach(progress => {
+    let speed = 1
+    let x = 0
+
+    // min of window position
+    let min_y = 670
+    // max of window position
+    let max_y = 3000
+    if (window.scrollY < min_y)
+      progress.style.top = "0%"
+    else if (window.scrollY >= min_y && window.scrollY <= max_y) {
+      x = (window.scrollY - min_y) * speed / (max_y - min_y) * 100
+      progress.style.top = `${x}%`
+    }
+    else
+      progress.style.top = "100%"
+
+    // else if (window.scrollY >= min_y) {
+    //   x = (window.scrollY - min_y) * speed / (max_y - min_y) * 100
+    //   progress.style.top = `${x}%`
+    // }
+
+  });
+}
+
+document.addEventListener("scroll", progress);
